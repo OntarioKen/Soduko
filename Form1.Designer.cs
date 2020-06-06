c737cc1f04b1a90c87d66d9ee72f619babcc3472
Ken Schirmeister <OntarioKen@gmail.com>
2020-06-05 23:31:32 -0400

    Done, 1st version!

---
+++ Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Soduko
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(377, 263);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 20;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 20;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 20;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Column4";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 20;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Column5";
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 20;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Column6";
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 20;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Column7";
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 20;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Column8";
+            this.Column8.Name = "Column8";
+            this.Column8.Width = 20;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "Column9";
+            this.Column9.Name = "Column9";
+            this.Column9.Width = 20;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 277);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Solve";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Solve);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(93, 277);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 2;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Location = new System.Drawing.Point(1, 8);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(377, 263);
+            this.panel1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 318);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Soduko";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Soduko
+{
+    public partial class Form1 : Form
+    {
+        static int[,] Defaults = {
+                                    { 7, 0, 6, 2, 0, 4, 0, 0, 0 },
+                                    { 0, 0, 0, 8, 0, 0, 3, 0, 0 },
+                                    { 5, 0, 0, 1, 0, 0, 0, 4, 0 },
+                                    { 9, 8, 7, 0, 4, 2, 0, 0, 5 },
+                                    { 0, 0, 0, 3, 0, 8, 0, 0, 0 },
+                                    { 3, 0, 0, 7, 1, 0, 2, 8, 6 },
+                                    { 0, 9, 0, 0, 0, 6, 0, 0, 8 },
+                                    { 0, 0, 2, 0, 0, 7, 0, 0, 0 },
+                                    { 0, 0, 0, 5, 0, 1, 9, 0, 2 }
+                                };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //================================================================================
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 9; i++)
+                dataGridView1.RowCount = 9;
+            dataGridView1.Rows[0].Cells[0].ValueType = typeof(int);
+            for (int i = 0; i < 9; i++)
+                for (int j = 0; j < 9; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].ValueType = typeof(int);
+                    if (Defaults[i, j] != 0)
+                        dataGridView1.Rows[i].Cells[j].Value = Defaults[i, j];
+                }
+        }
+
+        //================================================================================
+
+        internal int CellValue(
+                int iCol,
+                int iRow)
+        {
+            var cell = dataGridView1.Rows[iRow].Cells[iCol];
+            if (cell.Value == null)
+                return 0;
+            return (int)cell.Value;
+        }
+
+        //================================================================================
+
+        bool NextCell(
+                    int iCol,
+                    int iRow)
+        {
+            int iColNext = (iCol + 1) % 9;
+            int iRowNext = iRow;
+            if (iColNext == 0)
+                iRowNext++;
+            return FindNumberForThisCell(iColNext, iRowNext);
+        }
+
+        //================================================================================
+
+        internal bool FindNumberForThisCell(
+                int iCol,
+                int iRow)
+        {
+            if (iRow == 9)
+            {
+                dataGridView1.Update();
+                MessageBox.Show("This is a solution");
+                return false;
+            }
+
+            int iCellValue = CellValue(iCol, iRow);      
+            bool bOK = iCellValue != 0;                 // already has a valid value
+
+            if (bOK)
+            {
+                if (CellCanHaveThisValue(iCol, iRow, iCellValue))
+                    bOK = NextCell(iCol, iRow);
+                else
+                    throw new Exception(string.Format("Given number {0} at {1}/{2} is not possible", iCellValue, iCol, iRow));
+            }
+            else
+            {
+                for (int i = 1; i <= 9; i++)
+                {
+                    if (CellCanHaveThisValue(iCol, iRow, i))
+                    {
+                        dataGridView1.Rows[iRow].Cells[iCol].Value = i;
+                        bOK = NextCell(iCol, iRow);
+                        if (!bOK)
+                            dataGridView1.Rows[iRow].Cells[iCol].Value = null;
+                        dataGridView1.Update();
+                    }
+                }
+            }
+            return bOK;
+        }
+
+        //================================================================================
+
+        private bool CellCanHaveThisValue(int iCol, int iRow, int x)
+        {
+            bool bOK = true;
+
+            // is it already in this row?
+            for (int i = 0; i < 9 && bOK; i++)
+                if (i != iCol && CellValue(i, iRow) == x)
+                    bOK = false;
+
+            // is it already in this column?
+            for (int i = 0; i < 9 && bOK; i++)
+                if (i != iRow && CellValue(iCol, i) == x)
+                    bOK = false;
+
+            if (bOK)
+            {
+                int iColBlock = (iCol / 3) * 3;
+                int iRowBlock = (iRow / 3) * 3;
+                for (int i = 0; i < 3; i++)
+                    for (int j = 0; j < 3; j++)
+                        if (iColBlock + i != iCol && iRowBlock + j != iRow && CellValue(iColBlock + i, iRowBlock + j) == x)
+                            bOK = false;
+            }
+            return bOK;
+        }
+
+        //==============================================================================================
+
+        private void Solve(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 9; i++)
+                for (int j = 0; j < 9; j++)
+                {
+                    if (CellValue(j, i) == 0)
+                    {
+                        dataGridView1.Rows[i].Cells[j].Value = null;
+                        dataGridView1.Rows[i].Cells[j].Style.ForeColor = Color.Black;
+                    }
+                    else
+                        dataGridView1.Rows[i].Cells[j].Style.ForeColor = Color.Red;
+                    dataGridView1.Rows[i].Cells[j].Selected = false;
+                }
+
+            dataGridView1.Update();
+
+            try 
+            {
+                FindNumberForThisCell(0, 0);
+                dataGridView1.Update();
+                MessageBox.Show("No more solutions found");
+            }
+            catch (Exception ex)
+            {
+                dataGridView1.Update();
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //==============================================================================================
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 9; i++)
+                for (int j = 0; j < 9; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = null;
+                    dataGridView1.Rows[i].Cells[j].Style.ForeColor = Color.Black;
+                }
+            dataGridView1.Update();
+        }
+
+        //==============================================================================================
+
+    }
+}
